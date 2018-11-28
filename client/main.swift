d95482bc88f91918666682cfc2d19f2cd70bb8f9
Michael Roitzsch <reactorcontrol@icloud.com>
2018-11-28 16:43:22 +0100

    command line argument parser

--- client/main.swift
+++ client/main.swift
@@ -1,3 +1,49 @@
 import Foundation
 
-let arguments = Array(ProcessInfo.processInfo.arguments.dropFirst())
+enum ArgumentError: Error {
+	case unknown(_: String)
+	case missing(_: String)
+}
+
+do {
+	/* parse arguments */
+	let arguments = CommandLine.arguments.dropFirst()
+	var iterator = arguments.makeIterator()
+
+	var clientArgument: String?
+	var secretArgument: String?
+	var serverArgument: String?
+
+	while let argument = iterator.next() {
+		switch argument {
+		case "--client":
+			clientArgument = iterator.next()
+		case "--key":
+			secretArgument = iterator.next()
+		case "--url":
+			serverArgument = iterator.next()
+		default:
+			throw ArgumentError.unknown(argument)
+		}
+	}
+
+	// FIXME: clientArgument?.removeAll(where: { CharacterSet.alphanumerics.contains($0) })
+	while serverArgument?.hasSuffix("/") ?? false {
+		serverArgument = String(serverArgument!.dropLast())
+	}
+
+	guard let client = clientArgument else {
+		throw ArgumentError.missing("--client")
+	}
+	guard let secret = secretArgument else {
+		throw ArgumentError.missing("--key")
+	}
+	guard let server = serverArgument else {
+		throw ArgumentError.missing("--url")
+	}
+}
+catch let error as ArgumentError {
+	print(error)
+	print("Usage: SSHProxy --client <name> --key <secret> --url <url>")
+	exit(EX_USAGE)
+}
