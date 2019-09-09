5ec09a34eeb06013042b0e0bbd6ec60db427251e
Michael Roitzsch <reactorcontrol@icloud.com>
2019-09-09 10:47:26 +0200

    prevent warning about missing pkg-config

--- proxy/linux/shell.nix
+++ proxy/linux/shell.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
 let swift = import ~/.nix-defexpr/swift.nix;
 in (import ~/.nix-defexpr/local.nix).mkShellPlain {
-	bin = [ swift php openssh ];
+	bin = [ swift php pkg-config openssh ];
 	lib = [ libsodium.dev ];
 }
