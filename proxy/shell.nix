39904aa05b93cf774d3a8787f266c7aca4546532
Michael Roitzsch <reactorcontrol@icloud.com>
2019-10-27 21:45:09 +0100

    improve Linux build
    • binutils needed to get Nix’ wrapped linker
    • rpath needed to find libsodium

--- proxy/shell.nix
+++ proxy/shell.nix
@@ -1,6 +1,7 @@
 with import <nixpkgs> {};
 let swift = import ~/.nix-defexpr/swift.nix;
 in (import ~/.nix-defexpr/local.nix).mkShellPlain {
-	bin = [ php ] ++ stdenv.lib.optionals stdenv.isLinux [ swift pkg-config openssh ];
+	bin = [ php ] ++ stdenv.lib.optionals stdenv.isLinux [ swift binutils pkg-config openssh ];
 	lib = stdenv.lib.optionals stdenv.isLinux [ libsodium.dev ];
+	rpath = stdenv.lib.optionals stdenv.isLinux [ libsodium ];
 }
