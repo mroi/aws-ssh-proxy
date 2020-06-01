8f2a102da94e703843e3dcf50dc8d22c611bfa57
Michael Roitzsch <reactorcontrol@icloud.com>
2020-06-01 15:57:50 +0200

    everything but Darwin needs extra build tools
    just macOS comes with swift and ssh by default

--- proxy/shell.nix
+++ proxy/shell.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
 let swift = import ~/.nix-defexpr/swift.nix;
 in (import ~/.nix-defexpr/local.nix).mkShellPlain {
-	bin = [ php ] ++ stdenv.lib.optionals stdenv.isLinux [ swift binutils openssh ];
+	bin = [ php ] ++ stdenv.lib.optionals (!stdenv.isDarwin) [ swift binutils openssh ];
 }
