1729a4e13b07343f4e09dab5f93a7274a7f17e5c
Michael Roitzsch <reactorcontrol@icloud.com>
2021-03-07 19:12:27 +0100

    update to current toolchain version
    • fix package manager warning
    • handle Package.resolved file

--- proxy/Package.swift
+++ proxy/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 import PackageDescription
 
 func isLinux<T>(_ array: Array<T>) -> Array<T> {
@@ -12,7 +12,7 @@ func isLinux<T>(_ array: Array<T>) -> Array<T> {
 let package = Package(
 	name: "SSHProxy",
 	platforms: [
-		.macOS(.v10_15)
+		.macOS(.v11)
 	],
 	products: [
 		.executable(name: "ssh-connect", targets: ["Connect"]),
@@ -24,7 +24,7 @@ let package = Package(
 	targets: [
 		.target(name: "Connect", dependencies: ["SSHProxy"], path: "connect"),
 		.target(name: "Forward", dependencies: ["SSHProxy"], path: "forward"),
-		.target(name: "SSHProxy", dependencies: ["Sandbox"] + isLinux(["Crypto"]), path: "common", sources: ["proxy.swift"]),
-		.target(name: "Sandbox", path: "common", sources: ["sandbox.c"], publicHeadersPath: ".")
+		.target(name: "SSHProxy", dependencies: ["Sandbox"] + isLinux(["Crypto"]), path: "common", exclude: ["sandbox.c"], sources: ["proxy.swift"]),
+		.target(name: "Sandbox", path: "common", exclude: ["proxy.swift"], sources: ["sandbox.c"], publicHeadersPath: ".")
 	]
 )
