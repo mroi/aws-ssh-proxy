c8cb44cdd7b736597095c9b24021c280a7f1a0f8
Michael Roitzsch <reactorcontrol@icloud.com>
2021-03-29 21:34:34 +0200

    enable Nix flake builds on Darwin

--- flake.nix
+++ flake.nix
@@ -4,20 +4,42 @@
 		url = github:apple/swift-crypto;
 		flake = false;
 	};
-	outputs = { self, nixpkgs, swift-crypto }: {
-		defaultPackage.x86_64-linux =
-			with import nixpkgs { system = "x86_64-linux"; };
+	outputs = { self, nixpkgs, swift-crypto }:
+		let
+			ssh-proxy = system: with import nixpkgs {
+				inherit system;
+				overlays = [ (self: super: {
+					# add swift to Xcode wrapper
+					xcodeenv = super.xcodeenv // {
+						composeXcodeWrapper = version: (super.xcodeenv.composeXcodeWrapper version).overrideAttrs (attrs: {
+							buildCommand = attrs.buildCommand + ''
+								cat <<- "EOF" > $out/bin/swift
+									#!/bin/sh
+									exec /usr/bin/swift "$@" --disable-sandbox
+								EOF
+								chmod a+x $out/bin/swift
+							'';
+						});
+					};
+				})];
+			};
 			clangStdenv.mkDerivation {
 				name = "ssh-proxy-${lib.substring 0 8 self.lastModifiedDate}";
 				src = self;
-				nativeBuildInputs = [ swift ];
+				nativeBuildInputs =
+					lib.optional clangStdenv.isLinux swift ++
+					lib.optionals clangStdenv.isDarwin [
+						(xcodeenv.composeXcodeWrapper { version = "12.4"; })
+						xcbuild
+					];
 				patchPhase = ''
 					substituteInPlace proxy/Package.swift --replace 'url: "https://github.com/apple/swift-crypto.git"' 'path: "${swift-crypto}") //'
 					substituteInPlace proxy/common/proxy.swift --replace /usr/bin/ssh ${openssh}/bin/ssh
 				'';
-				makeFlags = [ "-C" "proxy" "DESTDIR=$(out)" ];
-				dontBuild = true;
-				installTargets = "all";
+				makeFlags = [ "-C" "proxy" "DESTDIR=$(out)" "ENDPOINT=" "SECRET=" "SERVER=" "USERNAME=" ];
 			};
-	};
+		in {
+			defaultPackage.x86_64-darwin = ssh-proxy "x86_64-darwin";
+			defaultPackage.x86_64-linux = ssh-proxy "x86_64-linux";
+		};
 }
--- proxy/shell.nix
+++
@@ -1,5 +0,0 @@
-with import <nixpkgs> {};
-let swift = import ~/.nix-defexpr/swift.nix;
-in (import ~/.nix-defexpr/local.nix).mkShellPlain {
-	bin = [ php ] ++ stdenv.lib.optionals (!stdenv.isDarwin) [ swift binutils openssh ];
-}
