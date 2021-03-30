029da38a6afe283c8d7e4906d26ee9edd297ff24
Michael Roitzsch <reactorcontrol@icloud.com>
2021-03-30 21:13:33 +0200

    naming package dependencies is not required

--- proxy/Package.swift
+++ proxy/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 		.executable(name: "ssh-forward", targets: ["Forward"])
 	],
 	dependencies: [
-		.package(name: "swift-crypto", url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
+		.package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
 	],
 	targets: [
 		.target(name: "Connect", dependencies: ["SSHProxy"], path: "connect"),
