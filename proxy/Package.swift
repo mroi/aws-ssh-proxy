21c7dda1561f5f4cd908328cc628bfe485ed1f28
Michael Roitzsch <reactorcontrol@icloud.com>
2022-03-13 14:02:46 +0100

    update Swift argument parser version

--- proxy/Package.swift
+++ proxy/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 		.executable(name: "ssh-forward", targets: ["Forward"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.4.0"),
+		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
 		.package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
 	],
 	targets: [
