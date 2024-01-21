cf134f2ad6befe676bb9f00535dc1c3647f3eabd
Michael Roitzsch <reactorcontrol@icloud.com>
2024-01-21 18:11:40 +0100

    flake: refactor flake
    • use traditional fetching for build-time dependencies
    • prevent re-imports of nixpkgs, which increases eval time
    • use isLinux/isDarwin from buildPlatform where appropriate
    • improve indentation

--- flake.nix
+++ flake.nix
@@ -1,43 +1,47 @@
 {
 	description = "connect machines over SSH using Amazon EC2 VMs";
-	inputs.swift-argument-parser = {
-		url = "github:apple/swift-argument-parser";
-		flake = false;
-	};
-	inputs.swift-crypto = {
-		url = "github:apple/swift-crypto";
-		flake = false;
-	};
-	outputs = { self, nixpkgs, swift-argument-parser, swift-crypto }:
-		let
-			ssh-proxy = system: with import nixpkgs {
-				inherit system;
-				overlays = [ (self: super: {
-					# add swift to Xcode wrapper
-					xcodeenv = super.xcodeenv // {
-						composeXcodeWrapper = version: (super.xcodeenv.composeXcodeWrapper version).overrideAttrs (attrs: {
-							buildCommand = attrs.buildCommand + ''
-								cat <<- "EOF" > $out/bin/swift
-									#!/bin/sh
-									exec /usr/bin/swift "$@" --disable-sandbox
-								EOF
-								chmod a+x $out/bin/swift
-							'';
-						});
-					};
-				})];
-			};
+	outputs = { self, nixpkgs }: let
+
+		ssh-proxy = system:
+			with nixpkgs.legacyPackages.${system}.extend (self: super: {
+				# add swift to Xcode wrapper
+				xcodeenv = super.xcodeenv // {
+					composeXcodeWrapper = version: (super.xcodeenv.composeXcodeWrapper version).overrideAttrs (attrs: {
+						buildCommand = attrs.buildCommand + ''
+							cat <<- "EOF" > $out/bin/swift
+								#!/bin/sh
+								exec /usr/bin/swift "$@" --disable-sandbox
+							EOF
+							chmod a+x $out/bin/swift
+						'';
+					});
+				};
+			});
 			clangStdenv.mkDerivation {
 				name = "ssh-proxy-${lib.substring 0 8 self.lastModifiedDate}";
 				src = self;
+				__noChroot = true;
 				nativeBuildInputs =
-					lib.optionals clangStdenv.isLinux [
+					lib.optionals clangStdenv.buildPlatform.isLinux [
 						swift swiftpm
-					] ++ lib.optionals clangStdenv.isDarwin [
-						(xcodeenv.composeXcodeWrapper { version = "14.2.1"; })
+					] ++ lib.optionals clangStdenv.buildPlatform.isDarwin [
+						(xcodeenv.composeXcodeWrapper { version = "14.2"; })
 						xcbuild
 					];
-				patchPhase = ''
+				patchPhase = let
+					swift-argument-parser = fetchFromGitHub {
+						owner = "apple";
+						repo = "swift-argument-parser";
+						rev = "1.3.0";
+						hash = "sha256-B4SwsR5v5dHaBZZMQsHmMh4oopkKWJgVl+k5yULaV3I=";
+					};
+					swift-crypto = fetchFromGitHub {
+						owner = "apple";
+						repo = "swift-crypto";
+						rev = "3.1.0";
+						hash = "sha256-3LS1QrhTevZs51/qtfuXPZpm62d4gEn8pqMsYfED0yM=";
+					};
+				in ''
 					ln -s ${swift-argument-parser} swift-argument-parser
 					ln -s ${swift-crypto} swift-crypto
 					substituteInPlace proxy/Package.swift --replace 'url: "https://github.com/apple/swift-argument-parser.git"' 'path: "../swift-argument-parser"), //'
@@ -45,22 +49,23 @@
 					substituteInPlace proxy/common/ssh.swift --replace /usr/bin/ssh ${openssh}/bin/ssh
 				'';
 				dontUseSwiftpmBuild = true;
-				makeFlags = [ "-C" "proxy" "PREFIX=$(out)" "LOCAL_ID=" "API_URL=" "API_KEY=" "USERNAME=" ];
+				makeFlags = [ "-C proxy" "DESTDIR=$(out)" "LOCAL_ID=" "API_URL=" "API_KEY=" "USERNAME=" ];
 			};
 
-			shell = system: with import nixpkgs { inherit system; };
+		shell = system:
+			with nixpkgs.legacyPackages.${system};
 			mkShellNoCC {
 				packages = [ php ] ++
 					lib.optionals stdenv.isLinux [ clang swift swiftpm openssh ];
 				shellHook = "test -r ~/.shellrc && . ~/.shellrc";
 			};
 
-		in {
-			packages.x86_64-darwin.default = ssh-proxy "x86_64-darwin";
-			packages.x86_64-linux.default = ssh-proxy "x86_64-linux";
-			packages.x86_64-darwin.ssh-proxy = ssh-proxy "x86_64-darwin";
-			packages.x86_64-linux.ssh-proxy = ssh-proxy "x86_64-linux";
-			devShells.x86_64-darwin.default = shell "x86_64-darwin";
-			devShells.x86_64-linux.default = shell "x86_64-linux";
-		};
+	in {
+		packages.x86_64-darwin.default = ssh-proxy "x86_64-darwin";
+		packages.x86_64-linux.default = ssh-proxy "x86_64-linux";
+		packages.x86_64-darwin.ssh-proxy = ssh-proxy "x86_64-darwin";
+		packages.x86_64-linux.ssh-proxy = ssh-proxy "x86_64-linux";
+		devShells.x86_64-darwin.default = shell "x86_64-darwin";
+		devShells.x86_64-linux.default = shell "x86_64-linux";
+	};
 }
